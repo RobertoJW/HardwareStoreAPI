d1b8d1c9eaa79b5aa4c22bc5bfd1a43c0182e5cf
DanielPajaron <dapamonti@gmail.com>
2025-05-28 12:53:34 +0200

    Private quitado del atributo contraseña en usuario

--- HardwareStoreAPI/Modelo/Usuario.cs
+++ HardwareStoreAPI/Modelo/Usuario.cs
@@ -14,7 +14,7 @@ namespace HardwareStoreAPI.Modelo
         public string email { get; set; }
         public string userName { get; set; }
         // la contraseña no puede ser modificado. 
-        public string password { get; private set; }
+        public string password { get; set; }
         // añadimos '?' para permitir valores nulos para fotos de perfil. 
         public string? UrlprofilePhoto { get; set; }
 
